f6ec14adf445250d9f3ebf44b5c905c0657e7121
adasunnylily <1134097761@qq.com>
2026-06-15 17:01:24 +0800

    fix: support prototype API from file pages

--- ui_prototype/server.py
+++ ui_prototype/server.py
@@ -37,6 +37,9 @@ class PrototypeGateway(SimpleHTTPRequestHandler):
         self._proxy()
 
     def end_headers(self) -> None:
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type, Authorization")
         self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
         self.send_header("Pragma", "no-cache")
         self.send_header("Expires", "0")
